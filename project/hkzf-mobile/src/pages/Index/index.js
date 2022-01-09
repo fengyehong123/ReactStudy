2d28a05fed6a3133734752e6c6f8ad7500cacf43
fengyehong123 <1355930128@qq.com>
2022-01-09 10:53:59 +0800

    update

--- project/hkzf-mobile/src/pages/Index/index.js
+++ project/hkzf-mobile/src/pages/Index/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 // 导入轮播图组件
-import { Carousel, Flex } from 'antd-mobile';
+import { Carousel, Flex, Grid, WingBlank } from 'antd-mobile';
 // 导入axios
 import axios from 'axios'
-// 导入当前组件的css文件
+// 导入当前组件的scss文件
 import './index.scss'
 
 // 导入导航菜单图片
@@ -47,12 +47,20 @@ export default class Index extends React.Component {
         swipers: [],
         // 轮播图加载完成的flag
         isSwiperLoaded: false,
+        // 租房小组的数据
+        groups: [],
+        // 最新资讯
+        news: []
     }
 
     // 钩子函数,装载完成,在render之后才会被调用
     componentDidMount() {
         // 一进入页面,就调用获取轮播图数据的方法
         this.getSwipers();
+        // 一进入页面,就调用获取租房小组的方法
+        this.getGroups();
+        // 一进入页面,就获取最新资讯
+        this.getNews();
     }
 
     // 获取轮播图数据的方法
@@ -66,6 +74,38 @@ export default class Index extends React.Component {
                 isSwiperLoaded: true,
             }
         })
+    }
+
+    // 获取租房小组数据的方法
+    async getGroups() {
+        // 调用API接口,获取小组数据
+        const res = await axios.get('http://localhost:8080/home/groups', {
+            params: {
+                area: 'AREA|88cff55c-aaa4-e2e0'
+            }
+        });
+
+        // 更新数据状态
+        this.setState(() => {
+            return {
+                // res.data.body: 后端获取到的接口数据
+                groups: res.data.body
+            }
+        });
+    }
+
+    // 获取最新资讯
+    async getNews() {
+        // 发送请求,获取数据
+        const urlSearchParams = new URLSearchParams([['area', 'AREA|88cff55c-aaa4-e2e0']]);
+        const res = await axios.get(`http://localhost:8080/home/news?${urlSearchParams.toString()}`);
+
+        // 更新状态
+        this.setState(() => {
+            return {
+                news: res.data.body
+            }
+        });
     }
 
     // 渲染轮播图
@@ -102,6 +142,24 @@ export default class Index extends React.Component {
         )
     }
 
+    // 渲染最新资讯
+    renderNews() {
+        return this.state.news.map(item => (
+            <div className="news-item" key={item.id}>
+                <div className="imgwrap">
+                    <img className="img" src={`http://localhost:8080${item.imgSrc}`} alt="" />
+                </div>
+                <Flex className="content" direction="column" justify="between">
+                    <h3 className="title">{item.title}</h3>
+                    <Flex className="info" justify="between">
+                        <span>{item.from}</span>
+                        <span>{item.date}</span>
+                    </Flex>
+                </Flex>
+            </div>
+        ))
+    }
+
     render() {
         return (
             <div className="index">
@@ -134,6 +192,45 @@ export default class Index extends React.Component {
                     {/* 使用Flex布局组件 */}
                     {this.renderNavs()}
                 </Flex>
+
+                {/* 租房小组 */}
+                <div className="group">
+                    <h3 className="group-title">
+                        租房小组 <span className="more">更多</span>
+                    </h3>
+                    {/* 宫格组件 */}
+                    <Grid
+                        // 宫格的数据
+                        data={this.state.groups} 
+                        // 宫格的列数
+                        columnNum={2}
+                        // 是否为正方形
+                        square={false}
+                        // 是否有边框
+                        hasLine={false}
+                        // 自定义的宫格渲染函数
+                        renderItem={(item) => (
+                            // item是group数据list中的一个元素
+                            <Flex className="group-item" justify="around" key={item.id}>
+                                <div className="desc">
+                                    <p className="title">{item.title}</p>
+                                    <span className="info">{item.desc}</span>
+                                </div>
+                                {/* 后端的图片地址 */}
+                                <img src={`http://localhost:8080${item.imgSrc}`} alt="" />
+                            </Flex>
+                        )} 
+                    />
+                </div>
+                
+                {/* 最新资讯 */}
+                <div className="news">
+                    <h3 className="group-title">最新资讯</h3>
+                    {/* WingBlank组件,用来设置页面两侧的留白 */}
+                    <WingBlank size="md">
+                        {this.renderNews()}
+                    </WingBlank>
+                </div>
             </div>
         );
     }
