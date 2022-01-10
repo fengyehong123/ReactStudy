26f65ef43ddd5a514092bc170b896a0d85954ff5
fengyehong123 <1355930128@qq.com>
2022-01-10 21:24:30 +0800

    add

--- project/hkzf-mobile/src/pages/CityList/index.js
+++ project/hkzf-mobile/src/pages/CityList/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { NavBar } from 'antd-mobile';
+
+import './index.scss'
 
 export default class CityList extends React.Component {
     render() {
-        return <div>城市选择页面</div>
+        return (
+            <div className="cityList">
+                <NavBar
+                    className="navbar"
+                    mode="light"
+                    icon={<i className="iconfont icon-back"/>}
+                    // 点击左侧的图标按钮,返回上一个页面
+                    onLeftClick={() => this.props.history.go(-1)}
+                >
+                    城市选择
+                </NavBar>
+            </div>
+        )
     }
 }
