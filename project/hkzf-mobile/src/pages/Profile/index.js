5fac7e8ead3e4f52d9a2fd1a6d6be6ea4cacecd3
fengyehong123 <1355930128@qq.com>
2022-02-15 22:00:24 +0800

    登录显示个人信息

--- project/hkzf-mobile/src/pages/Profile/index.js
+++ project/hkzf-mobile/src/pages/Profile/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
+// 导入宫格组件
 import { Grid, Button } from 'antd-mobile'
-
 import { BASE_URL } from '../../utils/url'
+import { API } from '../../utils/api'
+// 导入认证工具类
+import * as auth from '../../utils/auth'
 
 import styles from './index.module.css'
 
 // 菜单数据
 const menus = [
+  // 如果菜单是可以点击的,是有to属性的
   { id: 1, name: '我的收藏', iconfont: 'icon-coll', to: '/favorate' },
   { id: 2, name: '我的出租', iconfont: 'icon-ind', to: '/rent' },
   { id: 3, name: '看房记录', iconfont: 'icon-record' },
@@ -25,58 +29,113 @@ const menus = [
 const DEFAULT_AVATAR = BASE_URL + '/img/profile/avatar.png'
 
 export default class Profile extends Component {
+
+  state = {
+    // 用户是否登录
+    isLogin: auth.isAuth(),
+    // 用户的个人信息
+    userInfo: {
+      nickname: '',
+      avatar: ''
+    }
+  }
+
+  // 钩子函数,一进入页面就调用
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  // 获取用户个人资料
+  async getUserInfo() {
+
+    // 如果用户未登录
+    if (!this.state.isLogin) {
+      return;
+    }
+
+    // 如果用户已经登录,发送请求,获取用户资料
+    const res = await API.get('/user', {
+      headers: {
+        authorization: auth.getToken()
+      }
+    })
+
+    // 只有当状态值为200的时候,才表示请求成功
+    if (res.data.status === 200) {
+      const { nickname, avatar } = res.data.body;
+      this.setState(() => {
+        return {
+          userInfo: {
+            nickname,
+            avatar: BASE_URL + avatar
+          }
+        }
+      })
+    }
+  }
+
   render() {
     const { history } = this.props
+    const { isLogin, userInfo: { nickname, avatar } } = this.state;
 
     return (
       <div className={styles.root}>
+
         {/* 个人信息 */}
         <div className={styles.title}>
-          <img
-            className={styles.bg}
-            src={BASE_URL + '/img/profile/bg.png'}
-            alt="背景图"
-          />
+          <img className={styles.bg} src={BASE_URL + '/img/profile/bg.png'} alt="背景图" />
           <div className={styles.info}>
+            {/* 个人头像 */}
             <div className={styles.myIcon}>
-              <img className={styles.avatar} src={DEFAULT_AVATAR} alt="icon" />
+              {/* 登录之后显示用户头像,否则显示默认的头像 */}
+              <img className={styles.avatar} src={avatar || DEFAULT_AVATAR} alt="icon" />
             </div>
-            <div className={styles.user}>
-              <div className={styles.name}>游客</div>
-              {/* 登录后展示： */}
-              {/* <>
-                <div className={styles.auth}>
-                  <span onClick={this.logout}>退出</span>
-                </div>
-                <div className={styles.edit}>
-                  编辑个人资料
-                  <span className={styles.arrow}>
-                    <i className="iconfont icon-arrow" />
-                  </span>
-                </div>
-              </> */}
 
-              {/* 未登录展示： */}
-              <div className={styles.edit}>
-                <Button
-                  type="primary"
-                  size="small"
-                  inline
-                  onClick={() => history.push('/login')}
-                >
-                  去登录
-                </Button>
-              </div>
+            <div className={styles.user}>
+              <div className={styles.name}>{nickname || '游客'}</div>
+              {
+                isLogin ? (
+                  // 登录后展示
+                  <>
+                    <div className={styles.auth}>
+                      <span onClick={this.logout}>退出</span>
+                    </div>
+                    <div className={styles.edit}>
+                      编辑个人资料
+                      <span className={styles.arrow}>
+                        <i className="iconfont icon-arrow" />
+                      </span>
+                    </div>
+                  </>
+                ) : (
+                  // 未登录展示
+                  <div className={styles.edit}>
+                    <Button
+                      type="primary"
+                      size="small"
+                      inline
+                      onClick={() => history.push('/login')}
+                    >
+                      去登录
+                    </Button>
+                  </div>
+                )
+              }
             </div>
           </div>
         </div>
 
         {/* 九宫格菜单 */}
         <Grid
+          // 九宫格菜单的数据源
           data={menus}
+          // 一共三列
           columnNum={3}
+          // 不设置边框
           hasLine={false}
+          // 渲染九宫格中的每一项
           renderItem={item =>
+            // 如果项目中有to属性,就渲染Link组件,表示该菜单是可导航的
             item.to ? (
               <Link to={item.to}>
                 <div className={styles.menuItem}>
@@ -97,6 +156,7 @@ export default class Profile extends Component {
         <div className={styles.ad}>
           <img src={BASE_URL + '/img/profile/join.png'} alt="" />
         </div>
+
       </div>
     )
   }
